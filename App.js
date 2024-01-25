a985e240921bf1ed164f6243abda5deddc411012
rloo8 <cc1647@naver.com>
2024-01-25 13:18:59 +0900

    #2.5: 기본 layout 및 style 작성

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>30</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { flex: 1, backgroundColor: "#FFCD00" },
+  city: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
+  cityName: { fontSize: 40, fontWeight: "500" },
+  weather: { flex: 3 },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: { fontSize: 160, marginTop: 50 },
+  desc: { fontSize: 50, marginTop: -20 },
 });
